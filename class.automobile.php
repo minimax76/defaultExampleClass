ea8f685d67420c5856fc06b38fbbbadab33c229a
Massimo Minighin <info@studiominighin.com>
2013-01-15 00:02:12 -0800

    Create class.automobile.php

---
+++ class.automobile.php
@@ -0,0 +1,28 @@
+<?php
+class Automobile {
+  /*
+	var $colore_vernice;
+	var $cilindrata_motore;
+	var $cavalli_motore;
+	*/
+	
+	var $colore_vernice,
+		$cilindrata_motore,
+		$cavalli_motore;
+
+	function Accelera($percentualeAcceleratore) {
+		// ...
+		// ...
+	}
+	
+	function Frena($percentualeFreno,$frenoMotore) {
+		// ...
+		// ...
+	}
+	
+	function Sterza($verso, $angolo) {
+		// ...
+		// ...
+	}
+}
+?>
